bc53844b67f96132ea02b2fb9f88a765d8138f43
Kushal Gandhi <krgandh2@illinois.edu>
2016-11-12 16:39:19 -0600

    Fixed timestamps in database

--- Models.py
+++ Models.py
@@ -1,7 +1,7 @@
 import sqlite3 as sql
 import urllib2
 import json
-import time
+from datetime import datetime, timedelta
 
 database = "Building.db"
 
@@ -12,9 +12,15 @@ def insert_data():
 
 	with sql.connect(database) as con:
 	    cur = con.cursor()
+	    year = datetime.now().year
+	    month = datetime.now().month
+	    day = datetime.now().day
+	    hour = datetime.now().hour
 	    for lab in data:
 	    	lab_name = ' '.join(lab['strlabname'].split(' ')[1:])
 	    	if lab_name == '':
 	    		lab_name = 'default'
-	    	cur.execute("INSERT INTO Labs (LabName, InUse, Total, TS) VALUES (?,?,?,?)", (lab_name, lab['inusecount'], lab['machinecount'], time.time()))
+	    	cur.execute("INSERT INTO Labs (LabName, InUse, Total, Year, Month, Day, Hour) VALUES (?,?,?,?,?,?,?)", (lab_name, lab['inusecount'], lab['machinecount'], year, month, day, hour))
 	    con.commit()
+
+insert_data()
