bb2571e2eb951b784d4c6b4111983296bed821b3
Kushal Gandhi <krgandh2@illinois.edu>
2016-11-12 17:08:44 -0600

    Added long building names to database

--- Models.py
+++ Models.py
@@ -23,4 +23,30 @@ def insert_data():
 	    	cur.execute("INSERT INTO Labs (LabName, InUse, Total, Year, Month, Day, Hour) VALUES (?,?,?,?,?,?,?)", (lab_name, lab['inusecount'], lab['machinecount'], year, month, day, hour))
 	    con.commit()
 
-insert_data()
+def get_historical_data(lab_name):
+	total = 0
+	num = 0
+	average = [-1] * 24
+
+	for j in range(0, 24):
+		for i in range(0, 4):
+			d = datetime.today() + timedelta(hours=j) - timedelta(days=7*(i+1))
+			with sql.connect(database) as con:
+				cur = con.cursor()
+				result = cur.execute("SELECT InUse FROM Labs WHERE LabName = (?) AND Year = (?) AND Month = (?) AND Day = (?) AND Hour = (?);", (lab_name, d.year, d.month, d.day, d.hour))
+				con.commit()
+			result = result.fetchall()
+			if (len(result) > 0):
+				total += result[0][0]
+				num += 1
+
+		if num > 0:
+			average[j] = total / num
+		else:
+			print('No historical data available')
+
+	return average
+	
+
+# insert_data()
+get_historical_data('4th Floor Center')
