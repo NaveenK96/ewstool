3bbe391cd184ce010f0e6c79ba30db7923af4974
enunez9 <enunez9@illinois.edu>
2016-11-13 19:38:10 -0600

    Finished sidebar with favorites

--- Website.py
+++ Website.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, render_template, url_for, jsonify
+from flask import Flask, request, render_template, url_for, jsonify, make_response
+from collections import OrderedDict, defaultdict
+from datetime import datetime
 from Models import *
+from json import loads
 import sqlite3
 
 app = Flask(__name__)
-
-assignments = {}
 
 @app.context_processor
 def inject_static():
@@ -12,26 +13,90 @@ def inject_static():
                 js_url=url_for('static', filename='index.js'),
                 title="the bourne interface")
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     con = sqlite3.connect("Building.db")
     cursor = con.cursor()
-    cursor.execute("SELECT `BuildingName`, `Longitude`, `Latitude` FROM `Building` GROUP BY `BuildingName`")
-    data = cursor.fetchall()
-    print data
-    return render_template('index.html', data=jsonify(data))
 
-# @app.route('/<name>', methods=['GET', 'POST'])
-# def hello2(name=None):
-# 	# if request.method == 'POST':
-# 	# 	commentInfo = request.form.get('replyTo', None).split(" ")
-# 	# 	commentText = request.form.get('newcomment', None)
-# 	# 	con = sqlite3.connect("comments.db")
-# 	# 	cursor = con.cursor()
-# 	# 	insertComment(commentInfo, commentText, cursor)
-# 	# 	con.commit()
-# 	# 	con.close()
-# 	return render_template('files.html', name=assignments[name], comments=commentList)
+    if request.method == 'GET':
+        cursor.execute("SELECT `BuildingName`, `Latitude`, `Longitude` FROM `Building` GROUP BY `BuildingName`")
+        buildings = list()
+        for item in cursor.fetchall():
+            buildings.append({'name':item[0], 'lat':item[1], 'lng':item[2]})
+
+        return render_template('index.html', data=buildings)
+
+    _labs = dict()
+    cursor.execute("SELECT `LongName`, Labs.LabName, `InUse`, `Total`, `Latitude`, `Longitude` FROM `Labs` INNER JOIN `Building` ON Labs.LabName = Building.LabName ORDER BY Labs.ID DESC LIMIT 26")
+    for item in cursor.fetchall():
+        if not str(item[0]) in _labs:
+            _labs[str(item[0])] = dict()
+        _labs[str(item[0])][str(item[1])] = {'lab': str(item[1]), 'inuse': item[2], 'total': item[3], 'lat': item[4], 'lng': item[5]}
+
+    labs = OrderedDict()
+    for item in sorted(_labs.keys()):
+        labs[item] = _labs[item]
+
+    favorites = request.form.get('favorites')
+    building = request.form.get('building')
+    lab = request.form.get('lab')
+    option = request.form.get('option')
+
+    if not favorites:
+        favorites = dict()
+    else:
+        favorites = loads(favorites)
+
+    if building and option == 'ADD':
+        lab = lab.split(":", 1)[0]
+        if building not in favorites:
+            favorites[building] = list()
+        favorites[building].append(lab)
+
+    if building and option == 'REMOVE':
+        lab = lab.split(":", 1)[0]
+        lab = lab.split(" ")
+        lab = lab[len(lab) - 1]
+        print favorites
+        print favorites[building]
+        print lab
+        favorites[building].remove(lab)
+        if not favorites[building]:
+            del favorites[building]
+
+    to_del = list()
+    for item in favorites:
+        for thing in favorites[item]:
+            to_del.append([item, thing])
+
+    _favorites = dict()
+    for item in to_del:
+        if item[0] not in _favorites:
+            _favorites[item[0]] = dict()
+        _favorites[item[0]][item[1]] = labs[item[0]][item[1]]
+        del labs[item[0]][item[1]]
+
+    html = ""
+    if _favorites:
+        html += """<div class="panel panel-info"><div class="panel-heading"><h1 class=\"panel-title\">Favorites</h1></div>"""
+        for item in enumerate(_favorites):
+            num = str(item[0])
+            item = item[1]
+            for thing in _favorites[item]:
+                html += """<div class=\"panel-body\"><a><div><div class=\"hidden\" style=\"display: none\">%s</div><div class=\"lab\">%s</div><span class=\"fa fa-star pull-right\" title=\"Unfavorite\"></span></div></a></div>""" % (str(_favorites[item][thing]['lat']) + ',' + str(_favorites[item][thing]['lng']) + "," + item + ",", item + " " + _favorites[item][thing]['lab'] + ": " + str(_favorites[item][thing]['inuse']) + " / " + str(_favorites[item][thing]['total']))
+        html += """</div>"""
+
+    for item in enumerate(labs):
+        num = str(item[0])
+        item = item[1]
+        if labs[item]:
+            html += """<div class=\"panel panel-default\"><div class=\"panel-heading\" role=\"tab\" id=\"%s\"><h4 class=\"panel-title\"><a role=\"button\" data-toggle=\"collapse\" data-parent=\"#accordion\" href=\"#collapse%s\" aria-expanded=\"true\" aria-controls=\"collapse%s\">%s</a></h4></div><div id=\"collapse%s\" class=\"panel-collapse collapse out\" role=\"tabpanel\" aria-labelledby=\"%s\">""" % ('-'.join(item.split(' ')), num, num, item, num,'-'.join(item.split(' ')))
+        for thing in labs[item]:
+            html += """<div class=\"panel-body\"><a><div><div class=\"hidden\" style=\"display: none\">%s</div><div class=\"lab\">%s</div><span class=\"fa fa-star-o pull-right\" title=\"Favorite\"></span></div></a></div>""" % (str(labs[item][thing]['lat']) + ',' + str(labs[item][thing]['lng']) + "," + item + ",", labs[item][thing]['lab'] + ": " + str(labs[item][thing]['inuse']) + " / " + str(labs[item][thing]['total']))
+        html += """</div></div>"""
+
+    return jsonify({'html':html, 'favorites':favorites})
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    app.run(debug=True, ssl_context=('server.crt', 'server.key'))
--- static/index.js
+++ static/index.js
@@ -1,35 +1,165 @@
+var data = [];
+function initData(input) {
+    data = input;
+}
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.111674, lng: -88.227017},
+        zoom: 17,
+        mapTypeControl: false,
+        scaleControl: true,
+        disableDoubleClickZoom: true,
+        scrollwheel: false
+    });
+    data.map(function(item) {
+        var fillColor = "00FF00"
+        var pinImage = new google.maps.MarkerImage("https://chart.apis.google.com/chart?chst=d_map_spin&chld=.8|0|" + fillColor + "|9|_|" + item.name);
+        var marker = new google.maps.Marker({
+            position: {lat: item.lat, lng: item.lng},
+            map: map,
+            title: item.name,
+            icon:pinImage
+        });
+        var contentString = 'Hello';
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+        marker.addListener('mouseover', function() {
+          infowindow.open(map, marker);
+        });
+        marker.addListener('mouseout', function() {
+          infowindow.close(map, marker);
+        });
+    });
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        map.setCenter(pos);
+        }, function() {
+            // handleLocationError(true, infoWindow, map.getCenter());
+            // alert("Geolocation not supported.");
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        // handleLocationError(false, infoWindow, map.getCenter());
+        // alert("Geolocation not supported.");
+    }
+    if (typeof(Storage) !== "undefined") {
+        var favorites = localStorage.getItem("favorites");
+        $.post("/", {
+                favorites: favorites
+            },
+            function (data, status) {
+                if (status == 'success') {
+                    $('#accordion').append(data.html);
+                    $('.fa.fa-star-o').click(handlero);
+                    $('.fa.fa-star').click(handler);
+                }
+            }
+        );
+    }
+}
+
 $(function () {
     var $menu = $('#sidebar-wrapper');
     var $content = $('#main-wrapper');
     $content.addClass('no-transition');
     $menu.hide();
     $menu.css('left', -($menu.outerWidth() + 10));
-    $content.removeClass('col-md-10').addClass('col-md-12');
+    $content.removeClass('col-md-9').addClass('col-md-12');
     $('#toggle-button').click(function () {
         $content.removeClass('no-transition');
-        if ($menu.is(':visible') && $content.hasClass('col-md-10')) {
+        if ($menu.is(':visible') && $content.hasClass('col-md-9')) {
             // Slide out
             $menu.animate({
                 left: -($menu.outerWidth() + 10)
             }, function () {
                 $menu.hide(1000);
             });
-            $content.removeClass('col-md-10').addClass('col-md-12');
+            $content.removeClass('col-md-9').addClass('col-md-12');
         }
         else {
             // Slide in
-            $menu.show(500).animate({ left: 0 });
-            $content.removeClass('col-md-12').addClass('col-md-10');
+            $menu.show(0).animate({ left: 0 });
+            $content.removeClass('col-md-12').addClass('col-md-9');
         }
         if($content.hasClass('col-md-12') && $menu.is(':hidden')) {
         $menu.animate({
                 left: 0
             }, function () {
-                $menu.show(1000);
+                $menu.show(0);
             });
         //  $menu.show();
         $content.removeClass('no-transition');
-        $content.removeClass('col-md-12').addClass('col-md-10');
+        $content.removeClass('col-md-12').addClass('col-md-9');
+        }
+    });
+    $('.panel-body').click(function () {
+        var coords = $(this).find("div").text().split(",");
+        map.setCenter({'lat':parseFloat(coords[0]), 'lng':parseFloat(coords[1])})
+    });
+    $('#current-location').click(function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            map.setCenter(pos);
+            }, function() {
+                map.setCenter({lat: 40.111674, lng: -88.227017});
+                // alert("Geolocation not supported.");
+            });
+        } else {
+            map.setCenter({lat: 40.111674, lng: -88.227017});
+            // alert("Geolocation not supported.");
         }
     });
 });
+
+function handlero() {
+    var data = $(this).parent().children($('.hidden')).text().split(",");
+    var favorites = localStorage.getItem("favorites");
+    $.post("/", {
+            favorites: favorites,
+            building: data[2],
+            lab: data[3],
+            option: "ADD"
+        },
+        function (data, status) {
+            if (status == 'success') {
+                $('#accordion').empty();
+                $('#accordion').append(data.html);
+                $('.fa.fa-star-o').click(handlero);
+                $('.fa.fa-star').click(handler);
+                localStorage.setItem("favorites", JSON.stringify(data.favorites));
+            }
+        }
+    );
+}
+
+function handler() {
+    var data = $(this).parent().children($('.hidden')).text().split(",");
+    var favorites = localStorage.getItem("favorites");
+    $.post("/", {
+            favorites: favorites,
+            building: data[2],
+            lab: data[3],
+            option: "REMOVE"
+        },
+        function (data, status) {
+            if (status == 'success') {
+                $('#accordion').empty();
+                $('#accordion').append(data.html);
+                $('.fa.fa-star-o').click(handlero);
+                $('.fa.fa-star').click(handler);
+                localStorage.setItem("favorites", JSON.stringify(data.favorites));
+            }
+        }
+    );
+}
